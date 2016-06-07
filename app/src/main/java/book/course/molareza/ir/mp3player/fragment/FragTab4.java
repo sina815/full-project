ba167ec5746c92c1b3150380d84449af62c38777
sina <sina815@gmail.com>
2016-06-07 18:11:02 +0430

    v3 1395/3/18

---
+++ app/src/main/java/book/course/molareza/ir/mp3player/fragment/FragTab4.java
@@ -0,0 +1,32 @@
+package book.course.molareza.ir.mp3player.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import book.course.molareza.ir.mp3player.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragTab4 extends Fragment {
+
+
+    public FragTab4() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.frag_tab3, container, false);
+
+        return view;
+    }
+
+}
